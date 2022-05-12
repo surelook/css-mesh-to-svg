aad675c01150d3af8456aeea92241168a424d8dc
Marc Carroll <marc@deveire.com>
2022-05-12 15:39:11 +0100

    Adds cheeky click to download

--- main.js
+++ main.js
@@ -31,6 +31,37 @@ const render = () => {
         `
   
   document.body.append(svg)
+
+  svg.addEventListener('click', downloadSVGasTextFile)
+}
+
+function downloadSVGasTextFile() {
+  //get svg source.
+  var serializer = new XMLSerializer();
+  var source = serializer.serializeToString(svg);
+
+  //add name spaces.
+  if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
+    source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+  }
+  if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
+    source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+  }
+
+  //add xml declaration
+source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+
+//convert svg source to URI data scheme.
+var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
+
+
+  const a = document.createElement('a');
+  const e = new MouseEvent('click');
+  
+
+  a.download = 'download.svg';
+  a.href = url
+  a.dispatchEvent(e);
 }
 
 cssInput.addEventListener("input", render);
